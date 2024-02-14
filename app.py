85473743c2717158ec623da8351b53479c805bff
krishangupta33 <k.gupta035@gmail.com>
2024-02-14 03:36:35 -0500

    plotly plots

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from joblib import load
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 # Ensure all necessary NLTK data is downloaded
 nltk.download('averaged_perceptron_tagger')
@@ -33,35 +36,78 @@ def main():
 
     with tab1:
         st.title('Data Visualization and EDA')
-
+        st.markdown('---')
         # Load data
         og_data = pd.read_csv('questions.csv')
-
         # Display the first 5 rows of the data
-        st.write('First 5 rows of the data')
+        st.markdown("## First 5 rows of the data")
+        st.markdown("This table shows the first 5 rows of the data")
         st.write(og_data.head())
 
-        # Plot the distribution of question lengths
-        st.write('Distribution of Target Variable: Is Duplicate')
-        fig, ax = plt.subplots()
-        og_data['is_duplicate'].value_counts().plot(kind='bar', ax=ax)
-        st.pyplot(fig)
+        # Plot the distribution of question lengths using plotly
+        st.markdown('## Distribution of Target Variable')
+        fig = px.histogram(
+        og_data, 
+        x='is_duplicate', 
+        title='Distribution Plot',
+        labels={'is_duplicate': 'Is Duplicate'},  # Rename axis labels for clarity
+        text_auto=True,  # Automatically add text on bars
+        color='is_duplicate',  # Define the column based on which to color the bars
+        color_discrete_map={0: 'SkyBlue', 1: 'LightSalmon'}  # Custom colors for each value
+        )
+        fig.update_xaxes(
+        tickmode='array',
+        tickvals=[0, 1],
+        ticktext=['0', '1']
+        )
 
+        # Customize layout
+        fig.update_layout(
+            xaxis_title="Is Duplicate", 
+            yaxis_title="Count",
+            plot_bgcolor='rgba(0,0,0,0)'  # Make background color transparent
+        )
+
+        # Show the plot
+        st.plotly_chart(fig)
+
+
+
+
+      
+
+        # Plot the distribution of question lengths
 
         data = og_data.sample(n=10000, random_state=1)
         data = data.reset_index(drop=True)
 
-        # Plot the distribution of question lengths
-        st.write('\n\nDistribution of Question Lengths')
-        fig, ax = plt.subplots()
-        sns.histplot(data['question1'].apply(lambda x: len(x.split())), kde=True, ax=ax)
-        sns.histplot(data['question2'].apply(lambda x: len(x.split())), kde=True, ax=ax)
-        ax.set_xlabel('Question Length')
-        ax.set_ylabel('Frequency')
-        ax.legend(['Question 1', 'Question 2'])
-        st.pyplot(fig)
 
-        
+        st.markdown('## Distribution of Question Lengths')
+        data['Length of Question 1'] = data['question1'].apply(lambda x: len(x.split()))
+        data['Length of Question 2'] = data['question2'].apply(lambda x: len(x.split()))
+
+        # Create a single DataFrame for Plotly
+        df_long = pd.melt(data, value_vars=['Length of Question 1', 'Length of Question 2'], var_name='Question', value_name='Length')
+
+        # Plot using Plotly Express
+        fig = px.histogram(df_long, x='Length', color='Question', barmode='overlay',
+                       histnorm='',  # Use 'percent' for percentage, 'probability' for probability density, or '' for count
+                       marginal='box',  # Or 'rug', 'violin', 'box' for the marginal plots
+                       opacity=0.6,
+                       labels={'Length': 'Question Length'},
+                       title='Distribution of Question Lengths',
+                       color_discrete_map={
+                           'Length of Question 1': 'blue',  # Custom color for Question 1
+                           'Length of Question 2': 'red'    # Custom color for Question 2
+                       })
+        fig.update_layout(xaxis_title='Question Length', yaxis_title='Frequency')
+        fig.update_traces(marker_line_width=1,marker_line_color="black")
+
+        # Show plot
+        st.plotly_chart(fig)
+
+        ############################################################################################################
+        ############################################################################################################
         #Most common words
         questions = list(data['question1']) + list(data['question2'])
         len(questions)
@@ -75,24 +121,29 @@ def main():
         cnt = Counter()
         for word in questions:
             cnt.update(word.split())
-        print(cnt.most_common(10))
+        
 
         #plot
-        st.write('Most common words')
+        st.markdown('## Most common words')
         most_common = cnt.most_common(10)
         most_common = dict(most_common)
-        plt.figure(figsize=(12, 6))
-        plt.bar(most_common.keys(), most_common.values(), color='orange')
-        plt.title('Most common words')
-        plt.xlabel('Words')
-        plt.ylabel('Frequency')
-        plt.show()
-        st.pyplot(plt)
+        df_most_common = pd.DataFrame(list(most_common.items()), columns=['Word', 'Frequency'])
 
+        # Create a bar chart with Plotly Express
+        fig = px.bar(df_most_common, x='Word', y='Frequency', color='Frequency',
+                    color_continuous_scale='Oranges',  # Use a color scale based on frequency
+                    title='Most Common Words')
 
+        # Customize the layout
+        fig.update_layout(xaxis_title='Words', yaxis_title='Frequency',
+                        coloraxis_showscale=False)  # Hide the color scale bar
 
+        # Show the plot
+        st.plotly_chart(fig)
+
+        ############################################################################################################
+        ############################################################################################################
       # Plot the distribution of common words
-        st.write('Distribution of Common Words')
         def common_words(row):
             q1=set(map(lambda word: word.lower().strip(),row['question1'].split(" ")))
             q2=set(map(lambda word: word.lower().strip(),row['question2'].split(" ")))
@@ -100,16 +151,31 @@ def main():
 
         data['common_words'] = data.apply(common_words, axis=1)
 
-        plt.figure(figsize=(12, 6))
-        plt.subplot(1,2,1)
-        sns.violinplot(x = 'is_duplicate', y = 'common_words', data = data)
-        plt.title('Common words in questions')
-        plt.show()
-        st.pyplot(plt)
+        # Create a violin plot with Plotly Express
+        fig = px.violin(data, x='is_duplicate', y='common_words', color='is_duplicate',
+                        box=True,  # Display box plot inside the violin
+                        points="all",  # Display all points
+                        hover_data=data.columns,  # Show all data columns in hover info
+                        title='Common Words in Questions')
 
+        # Customize the layout for better readability
+        fig.update_layout(
+            xaxis_title='Is Duplicate',
+            yaxis_title='Number of Common Words',
+            legend_title='Duplicate Status'
+        )
 
+        # Customize the color scheme if desired
+        fig.update_traces(side='positive', line_color='blue', line_width=2)
+        fig.update_traces(side='negative', line_color='red', line_width=2)
+
+        # Display the plot in Streamlit
+        st.plotly_chart(fig)
+
+        ############################################################################################################
+        ############################################################################################################
         #Word length differences between questions
-        st.write('Word Length Differences')
+        st.markdown('## Word Length Differences')
 
         # calculate word length of questions in two columns
         data['q1_wordlen'] = data['question1'].apply(lambda x: len(str(x).split()))
@@ -118,12 +184,28 @@ def main():
         # create a new column that has difference in word length
         data['word_difference'] = abs(data['q1_wordlen'] - data['q2_wordlen'])
 
-        # plot to check the difference in word_difference across duplicate questions and non duplicate
-        plt.figure(figsize=(12, 6))
-        sns.violinplot(x = 'is_duplicate', y = 'word_difference', data = data)
-        plt.title('Word length difference in questions')
-        plt.show()
-        st.pyplot(plt)
+        # Create a violin plot with Plotly Express
+        fig = px.violin(data, x='is_duplicate', y='word_difference', color='is_duplicate',
+                        box=True,  # Display box plot inside the violin
+                        points="all",  # Display all points
+                        hover_data=data.columns,  # Show all data columns in hover info
+                        title='Word Length Difference in Questions')
+        
+        # Customize the layout
+        fig.update_layout(
+            xaxis_title='Is Duplicate',
+            yaxis_title='Word Length Difference',
+            legend_title='Duplicate Status'
+        )
+        
+        # Customize the color scheme if desired
+        fig.update_traces(marker=dict(opacity=0.5))
+        
+        # Display the plot in Streamlit
+        st.plotly_chart(fig)
+
+        ############################################################################################################
+        ############################################################################################################
 
 
 
@@ -304,12 +386,11 @@ def main():
         # Predict similarity
         if st.button('Predict Similarity'):
             probas = predict_duplicate_proba(question1, question2)
-            st.write('Probability of being non-duplicate: ', probas[0][0])
-            st.write('Probability of being duplicate: ', probas[0][1])
+            
             if probas[0][1] > 0.3:
-                st.write('The questions are similar')
+                st.markdown('The questions are similar')
             else:
-                st.write('The questions are not similar')
+                st.markdown('The questions are not similar')
 
     
 
